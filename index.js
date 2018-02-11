16a9c3634fe206ebb6dbe5c95591cc33b38a5c6d
Valtteri Pyyhtiä <valtteri.pyyhtia@gmail.com>
2018-02-11 12:48:18 +0200

    Testing simple http server

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML on helppoa',
+    date: '2017-12-10T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Selain pystyy suorittamaan vain javascriptiä',
+    date: '2017-12-10T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+    date: '2017-12-10T19:20:14.298Z',
+    important: true
+  }
+]
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
+})
+
+const port = 3001
+app.listen(port)
+console.log('Server running on port', port)
